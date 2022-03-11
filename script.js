5932ec389d435459fb460090b847621a6c8a68fc
filipmig <filipmigas91@gmail.com>
2022-03-11 11:05:07 +0100

    projectsJs04

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-const search = document.querySelector('.search');
-const li = document.querySelectorAll('li');
+const cookie = document.querySelector('img');
+const input = document.querySelector('input');
+const answer = document.querySelector('.answer');
+const error = document.querySelector('.error');
 
-const research = e => {
-    const text = e.target.value.toLowerCase();
+const answerArr = ['Tak!', 'Nie', 'Może', 'Ciężko powiedzieć...', 'Nie chcesz znać odpowiedzi na to pytanie... ;/'];
 
-    li.forEach( el => {
-        if(el.textContent.toLocaleLowerCase().indexOf(text) !== -1){
-            el.style.display = 'block';
-        } else {
-            el.style.display = 'none';
-        }
-    })
+const shakeCookie = () => {
+
+    cookie.classList.add('shake-animation');
+    setTimeout(checkInput, 1000);
 }
 
-search.addEventListener('keyup', research);
+const checkInput = () => {
+    if(input.value !== '' && input.value.slice(-1) === '?'){
+        generateAnswer();
+        error.textContent = '';
+    } else if (input.value !== '' && input.value.slice(-1) !== '?'){
+        error.textContent = 'Pytanie musi zakończone być znakiem "?"';
+        answer.textContent = '';
+    } else {
+        error.textContent = 'Musisz zadać jakieś pytanie!';
+        answer.textContent = '';
+    }
+    cookie.classList.remove('shake-animation');
+}
+const generateAnswer = () => {
+    const number = Math.floor(Math.random() * 5);
+    answer.innerHTML = `<span>Odpowiedź:</span> ${answerArr[number]}`;
+}
+
+cookie.addEventListener('click', shakeCookie);
